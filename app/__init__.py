a0d3f50b86c224293e5ef4906e7b75bf85c1e93f
Amanda <jonesamanda996@gmail.com>
2023-04-21 11:06:56 -0400

    crystal class and blueprint registered

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 def create_app():
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
-
+    from .routes import crystal_bp
+    app.register_blueprint(crystal_bp)
     return app
---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, jsonify
+
+class Crystal():
+    def __init__ (self, id, name, color, powers):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.powers = powers
+
+
+crystals = [
+    Crystal(1, "Amethyst", "Purple", "Infinite Knowledge and wisdom"),
+    Crystal(2, "Tigers Eye", "Gold", "Confidence, Strength"),
+    Crystal(3, "Rose Quartz", "Pink", "Love")
+]
+
+crystal_bp = Blueprint("crystals", __name__, url_prefix="/crystals")
+
+@crystal_bp.route("", methods=["GET"])
+def handle_crystals():
+    crystal_response = []
+    for crystal in crystals:
+        crystal_response.append({
+            "id": crystal.id,
+            "name": crystal.name,
+            "color": crystal.color,
+            "power": crystal.powers
+        })
+
+    return jsonify(crystal_response)
